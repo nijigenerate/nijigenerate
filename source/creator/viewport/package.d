df68eef674b31902284af70bb3cf1540b9633a59
seagetch <sigetch@gmail.com>
2024-05-01 22:29:16 +0900

    Changed the size of drag handles, and refactor the code to change size easily.

--- source/creator/viewport/package.d
+++ source/creator/viewport/package.d
@@ -25,6 +25,10 @@ import std.algorithm.sorting;
 import std.algorithm.searching;
 import std.stdio;
 
+private {
+    enum HandleSize = 24;
+}
+
 /**
     Draws the viewport contents
 */
@@ -479,8 +483,8 @@ void incViewportTransformHandle() {
 
 
     // Move handle
-    incBeginViewportToolArea(name, ImVec2(bounds.x - 32, bounds.y - 32));
-    igButton("", ImVec2(32, 32));
+    incBeginViewportToolArea(name, ImVec2(bounds.x - HandleSize, bounds.y - HandleSize));
+    igButton("", ImVec2(HandleSize, HandleSize));
     if (igIsItemHovered() && igIsMouseDown(btn)) {
         foreach (selectedNode; incSelectedNodes) {
             if (isOwned(selectedNode, incSelectedNodes))
@@ -610,8 +614,8 @@ void incViewportTransformHandle() {
     }
     // Scale handle
     if (incSelectedNodes.length == 1) {
-        incBeginViewportToolArea(name, ImVec2(bounds.x - 32, bounds.w));
-        igButton("", ImVec2(32, 32));
+        incBeginViewportToolArea(name, ImVec2(bounds.x - HandleSize, bounds.w));
+        igButton("", ImVec2(HandleSize, HandleSize));
         if (igIsItemHovered() && igIsMouseDown(btn)) {
             foreach (selectedNode; incSelectedNodes) {
                 if (isOwned(selectedNode, incSelectedNodes))
@@ -702,8 +706,8 @@ void incViewportTransformHandle() {
     }
     // Rotation handle
     if (incSelectedNodes.length == 1) {
-        incBeginViewportToolArea(name, ImVec2(bounds.z, bounds.y - 32));
-        igButton("", ImVec2(32, 32));
+        incBeginViewportToolArea(name, ImVec2(bounds.z, bounds.y - HandleSize));
+        igButton("", ImVec2(HandleSize, HandleSize));
         if (igIsItemHovered() && igIsMouseDown(btn)) {
             foreach (selectedNode; incSelectedNodes) {
                 if (isOwned(selectedNode, incSelectedNodes))
