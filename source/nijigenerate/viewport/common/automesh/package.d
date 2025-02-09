1c9016f77376b83bb927374c0f85fa118fe4a526
seagetch <sigetch@gmail.com>
2025-02-09 13:25:05 +0900

    WIP: implementing skeleton bone extractor as automesh algorithm.

--- source/nijigenerate/viewport/common/automesh/package.d
+++ source/nijigenerate/viewport/common/automesh/package.d
@@ -3,3 +3,4 @@ module nijigenerate.viewport.common.automesh;
 public import nijigenerate.viewport.common.automesh.automesh;
 public import nijigenerate.viewport.common.automesh.contours;
 public import nijigenerate.viewport.common.automesh.grid;
+public import nijigenerate.viewport.common.automesh.skelton_extractor;
