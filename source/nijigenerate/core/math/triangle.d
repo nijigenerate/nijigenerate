3dec5d201259a55b47c20607d8ceaf3c013778c9
seagetch <sigetch@gmail.com>
2025-02-26 08:15:45 +0900

    WIP: Implementing completeUncoveredArea (not tested yet.)

--- source/nijigenerate/core/math/triangle.d
+++ source/nijigenerate/core/math/triangle.d
@@ -2,9 +2,13 @@ module nijigenerate.core.math.triangle;
 
 import nijigenerate.core.math.vertex;
 import nijigenerate.viewport.common.mesh;
-import nijilive.math;
+import nijilive.math.triangle;
 import nijilive;
 
+import std.typecons;
+import std.algorithm;
+import std.array;
+import std.traits;
 
 Deformation* deformByDeformationBinding(T)(T[] vertices, DeformationParameterBinding binding, vec2u index, bool flipHorz = false) {
     import std.stdio;
@@ -211,4 +215,371 @@ Deformation* deformByDeformationBinding(T, S: PathDeformer)(T[] vertices, S defo
         newDeform.vertexOffsets ~= deformedVertices[i] - position(v);
     }
     return newDeform;
+}
+
+
+auto triangulate(T)(T[] vertices, vec4 bounds) {
+    vec2 min, max;
+    min = bounds.xy;
+    max = bounds.zw;
+
+    // Pad (fudge factors are a hack to work around contains() instability, TODO: fix)
+    vec2 range = max - min;
+    min -= range + vec2(range.y, range.x) + vec2(0.123, 0.125);
+    max += range + vec2(range.y, range.x) + vec2(0.127, 0.129);
+
+    vec3u[] tris;
+    vec3u[] tri2edge;
+    vec2u[] edge2tri;
+
+    vec2[] vtx;
+    vtx.length = 4;
+
+    // Define initial state (two tris)
+    vtx[0] = vec2(min.x, min.y);
+    vtx[1] = vec2(min.x, max.y);
+    vtx[2] = vec2(max.x, max.y);
+    vtx[3] = vec2(max.x, min.y);
+    tris ~= vec3u(0, 1, 3);
+    tris ~= vec3u(1, 2, 3);
+    tri2edge ~= vec3u(0, 1, 2);
+    tri2edge ~= vec3u(3, 4, 1);
+    edge2tri ~= vec2u(0, 0);
+    edge2tri ~= vec2u(0, 1);
+    edge2tri ~= vec2u(0, 0);
+    edge2tri ~= vec2u(1, 1);
+    edge2tri ~= vec2u(1, 1);
+
+    // Helpers
+    float sign(vec2 p1, vec2 p2, vec2 p3) {
+        return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+    }
+
+    bool contains(vec3u tri, vec2 pt) {
+        float d1, d2, d3;
+        bool hasNeg, hasPos;
+
+        d1 = sign(pt, vtx[tri.x], vtx[tri.y]);
+        d2 = sign(pt, vtx[tri.y], vtx[tri.z]);
+        d3 = sign(pt, vtx[tri.z], vtx[tri.x]);
+
+        hasNeg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+        hasPos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+        return !(hasNeg && hasPos);
+    }
+
+    void replaceE2T(ref vec2u e2t, uint from, uint to) {
+        if (e2t.x == from) {
+            e2t.x = to;
+            if (e2t.y == from) e2t.y = to;
+        } else if (e2t.y == from) {
+            e2t.y = to;
+        } else assert(false, "edge mismatch");
+    }
+
+    void orientTri(uint tri, uint edge) {
+        vec3u t2e = tri2edge[tri];
+        vec3u pt = tris[tri];
+        if (t2e.x == edge) {
+            return;
+        } else if (t2e.y == edge) {
+            tri2edge[tri] = vec3u(t2e.y, t2e.z, t2e.x);
+            tris[tri] = vec3u(pt.y, pt.z, pt.x);
+        } else if (t2e.z == edge) {
+            tri2edge[tri] = vec3u(t2e.z, t2e.x, t2e.y);
+            tris[tri] = vec3u(pt.z, pt.x, pt.y);
+        } else {
+            assert(false, "triangle does not own edge");
+        }
+    }
+
+    void splitEdges() {
+        uint edgeCnt = cast(uint)edge2tri.length;
+        for(uint e = 0; e < edgeCnt; e++) {
+            vec2u tr = edge2tri[e];
+
+            if (tr.x != tr.y) continue; // Only handle outer edges
+
+            orientTri(tr.x, e);
+
+            uint t1 = tr.x;
+            uint t2 = cast(uint)tris.length;
+            uint l = tris[t1].x;
+            uint r = tris[t1].y;
+            uint z = tris[t1].z;
+            uint m = cast(uint)vtx.length;
+            vtx ~= (vtx[l] + vtx[r]) / 2;
+
+            uint xe = cast(uint)edge2tri.length;
+            uint me = xe + 1;
+            uint re = tri2edge[t1].y;
+
+            tris[t1].y = m;
+            tri2edge[t1].y = me;
+            tris ~= vec3u(m, r, z);
+            tri2edge ~= vec3u(xe, re, me);
+            edge2tri ~= vec2u(t2, t2);
+            edge2tri ~= vec2u(t1, t2);
+            replaceE2T(edge2tri[re], t1, t2);
+        }
+    }
+
+    bool inCircle(vec2 pa, vec2 pb, vec2 pc, vec2 pd) {
+        debug(delaunay) writefln("in_circle(%s, %s, %s, %s)", pa, pb, pc, pd);
+        float adx = pa.x - pd.x;
+        float ady = pa.y - pd.y;
+        float bdx = pb.x - pd.x;
+        float bdy = pb.y - pd.y;
+
+        float adxbdy = adx * bdy;
+        float bdxady = bdx * ady;
+        float oabd = adxbdy - bdxady;
+
+        if (oabd <= 0) return false;
+
+        float cdx = pc.x - pd.x;
+        float cdy = pc.y - pd.y;
+
+        float cdxady = cdx * ady;
+        float adxcdy = adx * cdy;
+        float ocad = cdxady - adxcdy;
+
+        if (ocad <= 0) return false;
+
+        float bdxcdy = bdx * cdy;
+        float cdxbdy = cdx * bdy;
+
+        float alift = adx * adx + ady * ady;
+        float blift = bdx * bdx + bdy * bdy;
+        float clift = cdx * cdx + cdy * cdy;
+
+        float det = alift * (bdxcdy - cdxbdy) + blift * ocad + clift * oabd;
+
+        debug(delaunay) writefln("det=%s", det);
+        return det > 0;
+    }
+
+    splitEdges();
+    splitEdges();
+    splitEdges();
+    splitEdges();
+
+    uint dropVertices = cast(uint)vtx.length;
+
+    // Add vertices, preserving Delaunay condition
+    foreach(orig_i, vertex; vertices) {
+        uint i = cast(uint)orig_i + dropVertices;
+        debug(delaunay) writefln("Add @%d: %s", i, vertex.position);
+        vtx ~= vertex.position;
+        bool found = false;
+
+        uint[] affectedEdges;
+
+        foreach(a_, tri; tris) {
+            if (!contains(tri, vertex.position)) continue;
+
+            /*
+                        x
+                Y-----------------X
+                \`,            '/    XYZ = original vertices
+                \ `q   a   p' /     a = original triangle
+                    \  `,    '  /      bc = new triangles
+                    \   `i'   /       xyz = original edges
+                    y \ b | c / z      pqr = new edges
+                    \  r  /
+                        \ | /
+                        \|/
+                        Z
+            */
+
+            // Subdivide containing triangle
+            // New triangles
+            uint a = cast(uint)a_;
+            uint b = cast(uint)tris.length;
+            uint c = b + 1;
+            tris[a] = vec3u(tri.x, tri.y, i);
+            tris ~= vec3u(tri.y, tri.z, i); // b
+            tris ~= vec3u(tri.z, tri.x, i); // c
+
+            debug(delaunay) writefln("*** Tri %d: %s Edges: %s", a, tris[a], tri2edge[a]);
+
+            // New inner edges
+            uint p = cast(uint)edge2tri.length;
+            uint q = p + 1;
+            uint r = q + 1;
+
+            // Get outer edges
+            uint x = tri2edge[a].x;
+            uint y = tri2edge[a].y;
+            uint z = tri2edge[a].z;
+
+            // Update triangle to edge mappings
+            tri2edge[a] = vec3u(x, q, p);
+            tri2edge ~= vec3u(y, r, q);
+            tri2edge ~= vec3u(z, p, r);
+
+            debug(delaunay) writefln("  * Tri a %d: %s Edges: %s", a, tris[a], tri2edge[a]);
+            debug(delaunay) writefln("  + Tri b %d: %s Edges: %s", b, tris[b], tri2edge[b]);
+            debug(delaunay) writefln("  + Tri c %d: %s Edges: %s", c, tris[c], tri2edge[c]);
+
+            // Save new edges
+            edge2tri ~= vec2u(c, a);
+            edge2tri ~= vec2u(a, b);
+            edge2tri ~= vec2u(b, c);
+            debug(delaunay) writefln("  + Edg p %d: Tris %s", p, edge2tri[p]);
+            debug(delaunay) writefln("  + Edg q %d: Tris %s", q, edge2tri[q]);
+            debug(delaunay) writefln("  + Edg r %d: Tris %s", r, edge2tri[r]);
+
+            // Update two outer edges
+            debug(delaunay) writefln("  - Edg y %d: Tris %s", y, edge2tri[y]);
+            replaceE2T(edge2tri[y], a, b);
+            debug(delaunay) writefln("  + Edg y %d: Tris %s", y, edge2tri[y]);
+            debug(delaunay) writefln("  - Edg z %d: Tris %s", y, edge2tri[z]);
+            replaceE2T(edge2tri[z], a, c);
+            debug(delaunay) writefln("  + Edg z %d: Tris %s", z, edge2tri[z]);
+
+            // Keep track of what edges we have to look at
+            affectedEdges ~= [x, y, z, p, q, r];
+
+            found = true;
+            break;
+        }
+        if (!found) {
+            debug(delaunay) writeln("FAILED!");
+            break;
+        }
+
+        bool[] checked;
+        checked.length = edge2tri.length;
+
+        for (uint j = 0; j < affectedEdges.length; j++) {
+            uint e = affectedEdges[j];
+            vec2u t = edge2tri[e];
+
+            debug(delaunay) writefln(" ## Edge %d: T %s: %s %s", e, t, tris[t.x], tris[t.y]);
+
+            if (t.x == t.y) {
+                debug(delaunay) writefln("  + Outer edge");
+                continue; // Outer edge
+            }
+
+            // Orient triangles so 1st edge is shared
+            orientTri(t.x, e);
+            orientTri(t.y, e);
+
+            assert(tris[t.x].x == tris[t.y].y, "triangles do not share edge");
+            assert(tris[t.y].x == tris[t.x].y, "triangles do not share edge");
+
+            uint a = tris[t.x].x;
+            uint c = tris[t.x].y;
+            uint d = tris[t.x].z;
+            uint b = tris[t.y].z;
+
+            // Delaunay check
+            if (!inCircle(vtx[b], vtx[a], vtx[c], vtx[d])) {
+                // We're good
+                debug(delaunay) writefln("  + Meets condition");
+                continue;
+            }
+
+            debug(delaunay) writefln("  - Flip!");
+
+            // Flip edge
+            /*
+                c          c
+                /|\      r / \ q
+                / | \      / x \
+            d x|y b -> d-----b
+                \ | /      \ y /
+                \|/      s \ / p
+                a          a
+            */
+            uint r = tri2edge[t.x].y;
+            uint s = tri2edge[t.x].z;
+            uint p = tri2edge[t.y].y;
+            uint q = tri2edge[t.y].z;
+
+            tris[t.x] = vec3u(d, b, c);
+            tris[t.t] = vec3u(b, d, a);
+            tri2edge[t.x] = vec3u(e, q, r);
+            tri2edge[t.y] = vec3u(e, s, p);
+            replaceE2T(edge2tri[q], t.y, t.x);
+            replaceE2T(edge2tri[s], t.x, t.y);
+
+            // Mark it as checked
+            checked[e] = true;
+
+            // Check the neighboring edges
+            if (!checked[p]) affectedEdges ~= p;
+            if (!checked[q]) affectedEdges ~= q;
+            if (!checked[r]) affectedEdges ~= r;
+            if (!checked[s]) affectedEdges ~= s;
+        }
+    }
+    tris = tris.filter!((t)=>t.x >= dropVertices && t.y >= dropVertices && t.z >= dropVertices).array;
+    foreach (ref t; tris) {
+        t.x -= dropVertices;
+        t.y -= dropVertices;
+        t.z -= dropVertices;
+    }
+    return tuple(vtx[dropVertices..$], tris);
+}
+
+vec4 getBounds(T)(ref T vertices) {
+    vec4 bounds = vec4(float.max, float.max, -float.max, -float.max);
+    foreach (v; vertices) {
+        bounds = vec4(min(bounds.x, v.x), min(bounds.y, v.y), max(bounds.z, v.x), max(bounds.w, v.y));
+    }
+    bounds.x = floor(bounds.x);
+    bounds.y = floor(bounds.y);
+    bounds.z = ceil(bounds.z);
+    bounds.w = ceil(bounds.w);
+    return bounds;
+}
+
+void fillPoly(T, S, U, V)(T texture, ulong width, ulong height, vec4 bounds, S[] vertices , U[] indices, ulong index, V value) if (isNumeric!U) {
+    vec2[3] tvertices = [
+        vertices[indices[3*index]].position,
+        vertices[indices[3*index+1]].position,
+        vertices[indices[3*index+2]].position
+    ];
+
+    vec4 tbounds = getBounds(tvertices);
+    int bwidth  = cast(int)(ceil(tbounds.z) - floor(tbounds.x) + 1);
+    int bheight = cast(int)(ceil(tbounds.w) - floor(tbounds.y) + 1);
+    int top  = cast(int)floor(tbounds.y);
+    int left = cast(int)floor(tbounds.x);
+    foreach (y; 0..bheight) {
+        foreach (x; 0..bwidth) {
+            vec2 pt = vec2(left + x, top + y);
+            if (isPointInTriangle(pt, tvertices)) {
+                pt-= bounds.xy;
+                texture[cast(int)(pt.y * width + pt.x)] = value;
+            }
+        }
+    }
+}
+
+void fillPoly(T, S, U, V)(T texture, ulong width, ulong height, vec4 bounds, S[] vertices , U[] indices, ulong index, V value) if (is(U: vec3u)) {
+    vec2[3] tvertices = [
+        vertices[indices[index].x].position,
+        vertices[indices[index].y].position,
+        vertices[indices[index].z].position
+    ];
+
+    vec4 tbounds = getBounds(tvertices);
+    int bwidth  = cast(int)(ceil(tbounds.z) - floor(tbounds.x) + 1);
+    int bheight = cast(int)(ceil(tbounds.w) - floor(tbounds.y) + 1);
+    int top  = cast(int)floor(tbounds.y);
+    int left = cast(int)floor(tbounds.x);
+    foreach (y; 0..bheight) {
+        foreach (x; 0..bwidth) {
+            vec2 pt = vec2(left + x, top + y);
+            if (isPointInTriangle(pt, tvertices)) {
+                pt-= bounds.xy;
+                texture[cast(int)(pt.y * width + pt.x)] = value;
+            }
+        }
+    }
 }
