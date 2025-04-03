431a4f23addcd77bec9f9cd16fa4c258bd12a544
seagetch <sigetch@gmail.com>
2025-04-04 01:36:33 +0900

    Restored std.stdio for Linux/macOS in crashdump.d

--- source/nijigenerate/utils/crashdump.d
+++ source/nijigenerate/utils/crashdump.d
@@ -94,13 +94,20 @@ void crashdump(T...)(Throwable throwable, T state) {
     } catch (Exception ex) {
         version (Windows) {
         } else{
-        writeln("Failed to write crash dump" ~ ex.msg);
+            import std.stdio;
+            writeln("Failed to write crash dump" ~ ex.msg);
         }
     }
 
     // Use appropriate system method to notify user where crash dump is.
-    version(OSX) writeln(_("\n\n\n===   nijigenerate has crashed   ===\nPlease send us the nijigenerate-crashdump.txt file in ~/Library/Logs\nAttach the file as a git issue @ https://github.com/nijigenerate/nijigenerate/issues"));
-    else version(linux) writeln(_("\n\n\n===   nijigenerate has crashed   ===\nPlease send us the nijigenerate-crashdump.txt file in your log directory, XDG_STATE_HOME. For Flatpak, this is in ~/.var/app/nijigenerate.nijigenerate.\nAttach the file as a git issue @ https://github.com/nijigenerate/nijigenerate/issues"));
+    version(OSX) {
+        import std.stdio;
+        writeln(_("\n\n\n===   nijigenerate has crashed   ===\nPlease send us the nijigenerate-crashdump.txt file in ~/Library/Logs\nAttach the file as a git issue @ https://github.com/nijigenerate/nijigenerate/issues"));
+    }
+    else version(linux) {
+        import std.stdio;
+        writeln(_("\n\n\n===   nijigenerate has crashed   ===\nPlease send us the nijigenerate-crashdump.txt file in your log directory, XDG_STATE_HOME. For Flatpak, this is in ~/.var/app/nijigenerate.nijigenerate.\nAttach the file as a git issue @ https://github.com/nijigenerate/nijigenerate/issues"));
+    }
     else version(Windows) ShowMessageBox(
         _("The application has unexpectedly crashed\nPlease send the developers the nijigenerate-crashdump.txt which has been put on your desktop\nVia https://github.com/nijigenerate/nijigenerate/issues"),
         _("nijigenerate Crashdump")
