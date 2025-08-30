Inochi2D/inochi-creator

Ask Save when Closing Project (#441)
* Refactor ImportKeepHandler and DialogHandler

* Add Exit Dialog

* Refactor file Saving to Module

* Use Action Stack check the project changes

* Allow Configuring Exit Save Behavior

* Refactor decouple `class CloseAskHandler` and `incExit()`

* new project and ask the user to save it

* Refactor CloseAskHandler to save.d and update copyright

* Ask to save when closing project

* Refactor welcome.d using incFileOpen()

* resolves https://github.com/Inochi2D/inochi-creator/issues/145
* Allow users to configure exit behavior
* A window pops up when the project is closed, exited, or new.
  * NewProjectAskHandler, CloseAskHandler, ExitAskHandler
* Import pop-up windows is currently not supported. I think `kra.d` and `psd.d` shoud be refactored first.
  * https://github.com/nijigenerate/nijigenerate/pull/30
  * https://github.com/nijigenerate/nijigenerate/pull/29
* Pop-up window when "open" is not currently supported

* Update CloseAskHandler wording

source/creator/io/save.d
@@ -1,0 +1,198 @@
+/*
+    Copyright © 2020-2024, Inochi2D Project
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors:
+        Luna Nielsen
+        Lin, Yong Xiang <r888800009@gmail.com>
+*/
+
+module creator.io.save;
+import creator.windows;
+import creator.widgets.dialog;
+import creator.core;
+import creator;
+
+import tinyfiledialogs;
+import i18n;
+
+bool incFileOpen() {
+    const TFD_Filter[] filters = [
+        { ["*.inx"], "Inochi Creator Project (*.inx)" }
+    ];
+
+    string file = incShowOpenDialog(filters, _("Open..."));
+    // FileException should handle in incOpenProject, so we don't write try/catch here
+    if (file) return incOpenProject(file);
+
+    return false;
+}
+
+bool incFileSave() {
+    incPopWelcomeWindow();
+
+    // If a projeect path is set then the user has opened or saved
+    // an existing file, we should just override that
+    if (incProjectPath.length > 0) {
+        // TODO: do backups on every save?
+
+        incSaveProject(incProjectPath);
+        return true;
+    } else {
+        const TFD_Filter[] filters = [
+            { ["*.inx"], "Inochi Creator Project (*.inx)" }
+        ];
+
+        string file = incShowSaveDialog(filters, "", _("Save..."));
+        if (file) {
+            incSaveProject(file);
+            return true;
+        }
+    }
+    return false;
+}
+
+void incFileSaveAs() {
+    incPopWelcomeWindow();
+    const TFD_Filter[] filters = [
+        { ["*.inx"], "Inochi Creator Project (*.inx)" }
+    ];
+
+    string fname = incProjectPath().length > 0 ? incProjectPath : "";
+    string file = incShowSaveDialog(filters, fname, _("Save As..."));
+    if (file) incSaveProject(file);
+}
+
+string incGetSaveProjectOnClose() {
+    auto config = incSettingsGet!string("SaveProjectOnClose", "Ask");
+
+    // validate config
+    import std.algorithm : canFind;
+    auto keys = incGetSaveProjectOption().keys();
+    if (keys.canFind(config) == false) {
+        config = "Ask";
+        incSetSaveProjectOnClose(config);
+    }
+    return config;
+}
+
+string[string] incGetSaveProjectOption() {
+    string[string] options = [
+        "Ask": _("Always ask"),
+        "dontSave": _("Don't save"),
+        // maybe should not have "Save" option prevent users stuck when exit
+        // "Save": _("Save")
+    ];
+    return options;
+}
+
+
+void incSetSaveProjectOnClose(string select) {
+    incSettingsSet("SaveProjectOnClose", select);
+}
+
+/**
+    Handle New Project with save ask
+    NOTE: it is only called by UI, not by code
+*/
+void incNewProjectAsk() {
+    auto handler = new NewProjectAskHandler();
+    handler.start();
+}
+
+class NewProjectAskHandler : CloseAskHandler {
+    override
+    void onProjectClose() {
+        incNewProject();
+    }
+}
+
+/**
+    Handle exit with save ask
+    NOTE: it is only called by UI, not by code
+*/
+void incExitSaveAsk() {
+    ExitAskHandler handler = new ExitAskHandler();
+    handler.start();
+}
+
+class ExitAskHandler : CloseAskHandler {
+    override
+    void onProjectClose() {
+        incExit();
+    }
+}
+
+/**
+    Handle close project with save ask
+    NOTE: it is only called by UI, not by code
+*/
+void incCloseProjectAsk() {
+    CloseAskHandler handler = new CloseAskHandler();
+    handler.start();
+}
+
+void incCloseProject() {
+    // creating new project
+    incNewProject();
+    incPushWindow(new WelcomeWindow());
+}
+
+class CloseAskHandler : DialogHandler {
+    const(char)* INC_EXIT_ASK_DIALOG_NAME = "CloseAskDialog";
+
+    this () {
+        super(INC_EXIT_ASK_DIALOG_NAME);
+    }
+
+    override
+    bool onClickYes() {
+        if (incFileSave()) this.onProjectClose();
+        return true;
+    }
+
+    override
+    bool onClickNo() {
+        this.onProjectClose();
+        return true;
+    }
+
+    void onProjectClose() {
+        // override this function for different close behaviour
+        incCloseProject();
+    }
+
+    void show() {
+        incDialog(
+            INC_EXIT_ASK_DIALOG_NAME,
+            __("Save before closing?"),
+            _("Would you like to save your changes before closing?\n\nYou can change the default behaviour in the settings."),
+            DialogLevel.Info,
+            DialogButtons.Yes | DialogButtons.No | DialogButtons.Cancel
+        );
+    }
+
+    void start() {
+        if (!incIsProjectModified()) {
+            this.onClickNo();
+            return;
+        }
+
+        switch (incGetSaveProjectOnClose()) {
+            case "dontSave":
+                this.onClickNo();
+                return;
+            case "Save":
+                // maybe should not have "Save" option prevent users stuck when exit
+                this.onClickYes();
+                return;
+            case "Ask":
+                this.register();
+                this.show();
+                return;
+            default:
+                throw new Exception("Invalid save project on close setting");
+                return;
+        }
+    }
+}

source/creator/windows/settings.d
@@ -195,17 +195,17 @@ protected:
                             }
                         endSection();
 
-                        beginSection(__("Import behaviour"));
+                        beginSection(__("Import behaviour")); {
                             string[string] configShowing = [
                                 "Ask": _("Always ask"),
                                 "Preserve": _("Preserve"),
                                 "NotPreserve": _("Don't preserve")
                             ];
 
-                            string selected = configShowing[incGetKeepLayerFolder()];
+                            string selected = configShowing.get(incGetKeepLayerFolder(), "Ask");
                             string keepLayerFolder = incSettingsGet!string("KeepLayerFolder");
 
-                            if(igBeginCombo(__("Preserve structure"), selected.toStringz)) {
+                            if (igBeginCombo(__("Preserve structure"), selected.toStringz)) {
                                 foreach(key, displayName ; configShowing) {
                                     if (igSelectable(displayName.toStringz, keepLayerFolder == key)) 
                                         incSetKeepLayerFolder(key);
@@ -213,7 +213,23 @@ protected:
 
                                 igEndCombo();
                             }
+                        }
+                        endSection();
 
+                        beginSection(__("On close project")); {
+                            import creator.io.save;
+                            string[string] option = incGetSaveProjectOption();
+                            string selected = option.get(incGetSaveProjectOnClose(), "Ask");
+                            if (igBeginCombo(__("Save project"), selected.toStringz)) {
+                                foreach(key, displayName ; option) {
+                                    if (igSelectable(displayName.toStringz, selected == key))
+                                        incSetSaveProjectOnClose(key);
+                                }
+
+                                igEndCombo();
+                            }
+                            incTooltip(_("Should changes be saved automatically when closing a project?"));
+                        }
                         endSection();
                         break;
                     case SettingsPane.Viewport:

source/creator/windows/welcome.d
@@ -17,6 +17,7 @@ import i18n;
 import inochi2d;
 import creator.ver;
 import creator.io;
+import creator.io.save;
 import creator;
 import creator.config;
 import creator.widgets.dialog;
@@ -225,21 +226,13 @@ protected:
                         incDummy(ImVec2(0, 2));
                         igIndent();
                             if (incTextLinkWithIcon("", _("New..."))) {
-                                incNewProject();
+                                incNewProjectAsk();
                                 this.close();
                             }
 
                             if (incTextLinkWithIcon("", _("Open..."))) {
-                                const TFD_Filter[] filters = [
-                                    { ["*.inx"], "Inochi Creator Project (*.inx)" }
-                                ];
-
-                                string file = incShowOpenDialog(filters, _("Open..."));
-                                if (file) {
-                                    // FileException should handle in incOpenProject, so we don't write try/catch here
-                                    if (incOpenProject(file))
-                                        this.close();
-                                }
+                                if (incFileOpen())
+                                    this.close();
                             }
 
 

nijigenerate/nijigenerate

source/nijigenerate/io/save.d
@@ -1,0 +1,208 @@
+/*
+    Copyright © 2020-2024, Inochi2D Project
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+
+    Authors:
+        Luna Nielsen
+        Lin, Yong Xiang <r888800009@gmail.com>
+*/
+
+module nijigenerate.io.save;
+import nijigenerate.windows;
+import nijigenerate.widgets.dialog;
+import nijigenerate.core;
+import nijigenerate;
+
+import tinyfiledialogs;
+import i18n;
+
+/**
+    check project has changes
+*/
+bool incIsProjectModified() {
+    // TODO: we need more detailed check, maybe history action stack or tracking all changes
+    // currently just assume user history action stack should record all changes
+    // if not record, it is action stack bug
+    return !incIsActionStackEmpty();
+}
+
+bool incFileOpen() {
+    const TFD_Filter[] filters = [
+        { ["*.inx"], "nijigenerate Project (*.inx)" }
+    ];
+
+    string file = incShowOpenDialog(filters, _("Open..."));
+    // FileException should handle in incOpenProject, so we don't write try/catch here
+    if (file) return incOpenProject(file);
+
+    return false;
+}
+
+bool incFileSave() {
+    incPopWelcomeWindow();
+
+    // If a projeect path is set then the user has opened or saved
+    // an existing file, we should just override that
+    if (incProjectPath.length > 0) {
+        // TODO: do backups on every save?
+
+        incSaveProject(incProjectPath);
+        return true;
+    } else {
+        const TFD_Filter[] filters = [
+            { ["*.inx"], "nijigenerate Project (*.inx)" }
+        ];
+
+        string file = incShowSaveDialog(filters, "", _("Save..."));
+        if (file) {
+            incSaveProject(file);
+            return true;
+        }
+    }
+    return false;
+}
+
+void incFileSaveAs() {
+    incPopWelcomeWindow();
+    const TFD_Filter[] filters = [
+        { ["*.inx"], "nijigenerate Project (*.inx)" }
+    ];
+
+    string fname = incProjectPath().length > 0 ? incProjectPath : "";
+    string file = incShowSaveDialog(filters, fname, _("Save As..."));
+    if (file) incSaveProject(file);
+}
+
+string incGetSaveProjectOnClose() {
+    auto config = incSettingsGet!string("SaveProjectOnClose", "Ask");
+
+    // validate config
+    import std.algorithm : canFind;
+    auto keys = incGetSaveProjectOption().keys();
+    if (keys.canFind(config) == false) {
+        config = "Ask";
+        incSetSaveProjectOnClose(config);
+    }
+    return config;
+}
+
+string[string] incGetSaveProjectOption() {
+    string[string] options = [
+        "Ask": _("Always ask"),
+        "dontSave": _("Don't save"),
+        // maybe should not have "Save" option prevent users stuck when exit
+        // "Save": _("Save")
+    ];
+    return options;
+}
+
+
+void incSetSaveProjectOnClose(string select) {
+    incSettingsSet("SaveProjectOnClose", select);
+}
+
+/**
+    Handle New Project with save ask
+    NOTE: it is only called by UI, not by code
+*/
+void incNewProjectAsk() {
+    auto handler = new NewProjectAskHandler();
+    handler.start();
+}
+
+class NewProjectAskHandler : CloseAskHandler {
+    override
+    void onProjectClose() {
+        incNewProject();
+    }
+}
+
+/**
+    Handle exit with save ask
+    NOTE: it is only called by UI, not by code
+*/
+void incExitSaveAsk() {
+    ExitAskHandler handler = new ExitAskHandler();
+    handler.start();
+}
+
+class ExitAskHandler : CloseAskHandler {
+    override
+    void onProjectClose() {
+        incExit();
+    }
+}
+
+/**
+    Handle close project with save ask
+    NOTE: it is only called by UI, not by code
+*/
+void incCloseProjectAsk() {
+    CloseAskHandler handler = new CloseAskHandler();
+    handler.start();
+}
+
+void incCloseProject() {
+    // creating new project
+    incNewProject();
+    incPushWindow(new WelcomeWindow());
+}
+
+class CloseAskHandler : DialogHandler {
+    const(char)* INC_EXIT_ASK_DIALOG_NAME = "CloseAskDialog";
+
+    this () {
+        super(INC_EXIT_ASK_DIALOG_NAME);
+    }
+
+    override
+    bool onClickYes() {
+        if (incFileSave()) this.onProjectClose();
+        return true;
+    }
+
+    override
+    bool onClickNo() {
+        this.onProjectClose();
+        return true;
+    }
+
+    void onProjectClose() {
+        // override this function for different close behaviour
+        incCloseProject();
+    }
+
+    void show() {
+        incDialog(
+            INC_EXIT_ASK_DIALOG_NAME,
+            __("Save before closing?"),
+            _("Would you like to save your changes before closing?\n\nYou can change the default behaviour in the settings."),
+            DialogLevel.Info,
+            DialogButtons.Yes | DialogButtons.No | DialogButtons.Cancel
+        );
+    }
+
+    void start() {
+        if (!incIsProjectModified()) {
+            this.onClickNo();
+            return;
+        }
+
+        switch (incGetSaveProjectOnClose()) {
+            case "dontSave":
+                this.onClickNo();
+                return;
+            case "Save":
+                // maybe should not have "Save" option prevent users stuck when exit
+                this.onClickYes();
+                return;
+            case "Ask":
+                this.register();
+                this.show();
+                return;
+            default:
+                throw new Exception("Invalid save project on close setting");
+                return;
+        }
+    }
+}

source/nijigenerate/windows/settings.d
@@ -210,25 +210,41 @@ protected:
                             }
                         endSection();
 
-                        beginSection(__("Import behaviour"));
+                        beginSection(__("Import behaviour")); {
                             string[string] configShowing = [
                                 "Ask": _("Always ask"),
                                 "Preserve": _("Preserve"),
                                 "NotPreserve": _("Don't preserve")
                             ];
 
-                            string selected = configShowing[incGetKeepLayerFolder()];
+                            string selected = configShowing.get(incGetKeepLayerFolder(), "Ask");
                             string keepLayerFolder = incSettingsGet!string("KeepLayerFolder");
 
-                            if(igBeginCombo(__("Preserve structure"), selected)) {
+                            if (igBeginCombo(__("Preserve structure"), selected.toStringz)) {
                                 foreach(key, displayName ; configShowing) {
-                                    if (igSelectable(displayName, keepLayerFolder == key))
+                                    if (igSelectable(displayName.toStringz, keepLayerFolder == key))
                                         incSetKeepLayerFolder(key);
                                 }
 
                                 igEndCombo();
                             }
+                        }
+                        endSection();
 
+                        beginSection(__("On close project")); {
+                            import nijigenerate.io.save;
+                            string[string] option = incGetSaveProjectOption();
+                            string selected = option.get(incGetSaveProjectOnClose(), "Ask");
+                            if (igBeginCombo(__("Save project"), selected.toStringz)) {
+                                foreach(key, displayName ; option) {
+                                    if (igSelectable(displayName.toStringz, selected == key))
+                                        incSetSaveProjectOnClose(key);
+                                }
+
+                                igEndCombo();
+                            }
+                            incTooltip(_("Should changes be saved automatically when closing a project?"));
+                        }
                         endSection();
                         break;
                     case SettingsPane.Viewport:

source/nijigenerate/windows/welcome.d
@@ -19,6 +19,7 @@ import i18n;
 import nijilive;
 import nijigenerate.ver;
 import nijigenerate.io;
+import nijigenerate.io.save;
 import nijigenerate;
 import nijigenerate.config;
 import nijigenerate.widgets.dialog;
@@ -217,21 +218,13 @@ protected:
                         incDummy(ImVec2(0, 2));
                         igIndent();
                             if (incTextLinkWithIcon("", _("New..."))) {
-                                incNewProject();
+                                incNewProjectAsk();
                                 this.close();
                             }
 
                             if (incTextLinkWithIcon("", _("Open..."))) {
-                                const TFD_Filter[] filters = [
-                                    { ["*.inx"], "nijigenerate Project (*.inx)" }
-                                ];
-
-                                string file = incShowOpenDialog(filters, _("Open..."));
-                                if (file) {
-                                    // FileException should handle in incOpenProject, so we don't write try/catch here
-                                    if (incOpenProject(file))
-                                        this.close();
-                                }
+                                if (incFileOpen())
+                                    this.close();
                             }
 
 