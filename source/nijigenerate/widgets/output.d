a5b2f080c6981605cd87ebc94aab3c728725e58e
seagetch <sigetch@gmail.com>
2025-08-31 18:29:27 +0900

    Removed TreeStore from source/nijigenerate/widgets/output.d to source/nijigenerate/core/selector/treestore.d

--- source/nijigenerate/widgets/output.d
+++ source/nijigenerate/widgets/output.d
@@ -192,73 +192,15 @@ interface Output {
     void reset();
 }
 
-class TreeStore {
-public:
-    CommandIssuer panel;
-
-    Resource[] nodes;
-    Resource[][Resource] children;
-    Resource[] roots;
-    bool[Resource] nodeIncluded;
-
-    void reset() {
-        children.clear();
-        nodes.length = 0;
-        nodeIncluded.clear();
-        roots.length = 0;
-    }
-
-    void setResources(Resource[] nodes_) {
-        nodes = nodes_;
-        roots.length = 0;
-        children.clear();
-        nodeIncluded.clear();
-        Resource[Resource] parentMap;
-        bool[Resource] rootMap;
-        bool[Resource][Resource] childMap;
-        foreach (n; nodes) {
-            nodeIncluded[n] = true;
-        }
-
-        void addToMap(Resource res, int level = 0) {
-            if (res in parentMap) return;
-            auto source = res.source;
-            while (source) {
-                if (source.source is null) break;
-                if (source in nodeIncluded || source.explicit) break;
-                source = source.source;
-            }
-            if (source) {
-                parentMap[res] = source;
-                childMap.require(source);
-                childMap[source][res] = true;
-                addToMap(source, level + 1);
-            } else {
-                rootMap[res] = true;
-            }
-        }
-
-        foreach (res; nodes) {
-            addToMap(res);
-        }
-        foreach (res; childMap.keys) {
-            if (res !in parentMap || parentMap[res] is null) {
-                rootMap[res] = true;
-            }
-        }
-        roots = rootMap.keys.sort!((a,b)=>a.index<b.index).array;
-        foreach (item; childMap.byKeyValue) {
-            children[item.key] = item.value.keys.sort!((a,b)=>a.index<b.index).array;
-        }
-    }
-}
+// TreeStore moved to nijigenerate.core.selector.treestore
+import nijigenerate.core.selector.treestore : TreeStore;
 
 class ListOutput : Output {
 protected:
     TreeStore self;
+    CommandIssuer panel;
 
     int IconSize = 20;
-    ref CommandIssuer panel() { return self.panel; }
     Resource popupOpened = null;
     InspectorHolder!Node activeInspector = null;
 
