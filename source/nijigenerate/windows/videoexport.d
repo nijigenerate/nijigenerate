4516251827a72846737496e9757415b5c661ac93
Lin, Yong Xiang <r888800009@gmail.com>
2025-09-02 18:03:38 +0800

    Add validation for camera size to prevent crashes and close pipe for Video Export
    from upstream fix
    * https://github.com/Inochi2D/inochi-creator/pull/411
    
    commits
    * Add validation for camera size to prevent ffmpeg crashes
    * Close ffmpeg pipe on click close button
    * Refactoring and setting steps

--- source/nijigenerate/windows/videoexport.d
+++ source/nijigenerate/windows/videoexport.d
@@ -218,6 +218,10 @@ protected:
                     incEndCategory();
                 }
             }
+
+            // assert user camera size is not odd, it will cause ffmpeg to fail and crash
+            bool userAssertOdd = incVerifyCameraSizeShowWarning(selectedCamera);
+
             igEndChild();
         igEndDisabled();
 
@@ -227,7 +231,11 @@ protected:
             igSameLine(0, 4);
 
             if (!vctx) {
-                if (incButtonColored(__("Export"), ImVec2(64, 24))) {
+                igBeginDisabled(userAssertOdd);
+                    bool clicked = incButtonColored(__("Export"), ImVec2(64, 24));
+                igEndDisabled();
+
+                if (clicked) {
                     playback = player.createOrGet(animToExport);
 
                     loops = clamp(loops, 1, int.max);
@@ -269,6 +277,7 @@ protected:
                     }
                 } else {
                     if (incButtonColored(__("Close"), ImVec2(64, 24))) {
+                        vctx.end();
                         this.close();
                     }
                 }
