32e3fff9332d571bf8c4f63426d177c7621a5acc
seagetch <sigetch@gmail.com>
2025-09-28 01:10:46 +0900

    fix: brush: force difference blend

--- source/nijigenerate/viewport/common/mesheditor/brushstate.d
+++ source/nijigenerate/viewport/common/mesheditor/brushstate.d
@@ -1,6 +1,6 @@
 module nijigenerate.viewport.common.mesheditor.brushstate;
 
-import nijilive.core.nodes.part : Part;
+import nijilive.core.nodes.part : Part, BlendMode;
 import nijigenerate.core.window : incViewportSetTemporaryBackgroundColor, incViewportClearTemporaryBackgroundColor;
 
 alias TeacherPart = Part;
@@ -8,8 +8,11 @@ alias TeacherPart = Part;
 private __gshared TeacherPart brushTeacherPart;
 private __gshared bool brushTeacherPartVisibilityOverridden;
 private __gshared bool brushTeacherPartPreviousEnabled;
+private __gshared BlendMode brushTeacherPartPreviousBlendMode;
+private __gshared bool brushTeacherPartBlendModeOverridden;
 
 TeacherPart incBrushGetTeacherPart() {
+    enforceTeacherPartBlendMode(brushTeacherPart);
     return brushTeacherPart;
 }
 
@@ -22,6 +25,7 @@ void incBrushSetTeacherPart(TeacherPart part) {
                 part.setEnabled(true);
                 brushTeacherPartVisibilityOverridden = true;
             }
+            enforceTeacherPartBlendMode(part);
             incViewportSetTemporaryBackgroundColor(0.0f, 0.0f, 0.0f, 1.0f);
         } else {
             incViewportClearTemporaryBackgroundColor();
@@ -29,10 +33,16 @@ void incBrushSetTeacherPart(TeacherPart part) {
         return;
     }
 
-    if (brushTeacherPart !is null && brushTeacherPartVisibilityOverridden) {
-        brushTeacherPart.setEnabled(brushTeacherPartPreviousEnabled);
+    if (brushTeacherPart !is null) {
+        if (brushTeacherPartVisibilityOverridden) {
+            brushTeacherPart.setEnabled(brushTeacherPartPreviousEnabled);
+        }
+        if (brushTeacherPartBlendModeOverridden) {
+            brushTeacherPart.blendingMode = brushTeacherPartPreviousBlendMode;
+        }
     }
     brushTeacherPartVisibilityOverridden = false;
+    brushTeacherPartBlendModeOverridden = false;
 
     brushTeacherPart = part;
 
@@ -42,9 +52,21 @@ void incBrushSetTeacherPart(TeacherPart part) {
             part.setEnabled(true);
             brushTeacherPartVisibilityOverridden = true;
         }
+        enforceTeacherPartBlendMode(part);
         incViewportSetTemporaryBackgroundColor(0.0f, 0.0f, 0.0f, 1.0f);
     } else {
         incViewportClearTemporaryBackgroundColor();
+    }
+}
+
+private void enforceTeacherPartBlendMode(TeacherPart part) {
+    if (part is null)
+        return;
+
+    if (part.blendingMode != BlendMode.Difference) {
+        brushTeacherPartPreviousBlendMode = part.blendingMode;
+        brushTeacherPartBlendModeOverridden = true;
+        part.blendingMode = BlendMode.Difference;
     }
 }
 
